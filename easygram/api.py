8ba976187cbe909bda676bc24d3e3eed15a4f1e3
August Corvin <255298133+AugustCorvin@users.noreply.github.com>
2026-01-16 22:48:08 +0300

    Исправление ошибки

--- easygram/api.py
+++ easygram/api.py
@@ -4,9 +4,9 @@ import requests
 
 class TelegramAPI:
     def __init__(self, token: str):
-        self.base_url = f"https:api.telegram.org/bot{token}"
+        self.base_url = f"https://api.telegram.org/bot{token}"
 
     def request(self, method: str, params: dict | None = None) -> dict:
         url = f"{self.base_url}/{method}"
-        response = request.post(url, json=params)
+        response = requests.post(url, json=params)
         return response.json()
