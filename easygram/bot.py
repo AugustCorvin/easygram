b4645f6006928965d2a09c636d41faa85366004a
August Corvin <255298133+AugustCorvin@users.noreply.github.com>
2026-01-17 11:37:29 +0300

    Переработка структуры

--- easygram/bot.py
+++ easygram/bot.py
@@ -3,8 +3,15 @@
 
 
 
-# Импортируем TelegramAPI
+# Импортируем TelegramAPI и time
 from .api import TelegramAPI
+import time
+
+# Создаем класс Message
+class Message:
+    def __init__(self, update):
+        self.text = update['message'].get('text', '')
+        self.user_id = update['message']['chat']['id']
 
 # Создаем класс бота
 class Bot:
@@ -13,6 +20,12 @@ class Bot:
         # Сохраняем API, начинаем получать обновления с самого начала
         self.api = TelegramAPI(token)
         self.offset = 0
+        self._handlers = []   # Список функций-обработчиков
+
+    # Декоратор для регистрации обработчиков
+    def new_message(self, func):
+        self._handlers.append(func)
+        return func
 
     # Получение новых обновлений
     def get_updates(self):
@@ -35,3 +48,14 @@ class Bot:
     # Отправка сообщений
     def send_message(self, chat_id: int, text: str):
         return self.api.request('sendMessage', {'chat_id': chat_id, 'text': text})
+
+    # Запуск бесконечного цикла
+    def run(self):
+        while True:
+            updates = self.get_updates()
+            for update in updates:
+                if 'message' in update:
+                    message = Message(update)
+                    for handler in self._handlers:
+                        handler(message)
+            time.sleep(1)
